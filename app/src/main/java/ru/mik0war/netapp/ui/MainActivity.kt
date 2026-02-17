9ba16c2d571dee21825995fd1a5051ec8867ff87
korneevDev <korneevDev@gmail.com>
2026-02-17 10:30:25 +0300

    Add theme 2

--- app/src/main/java/ru/mik0war/netapp/ui/MainActivity.kt
+++ app/src/main/java/ru/mik0war/netapp/ui/MainActivity.kt
@@ -6,16 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ru.mik0war.netapp.R
+import ru.mik0war.netapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+        Объявление переменной все функции onCreate
+        для использования её в рамках всего класса
+    */
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Инициализация переменной
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // Отображение корневого элемента.
+        // В случае, если оставить старый вариант,
+        // все настройки элментов, сделанные через
+        // биндинг не будут применены к отображаемым
+        // элементам
+        setContentView(binding.root)
     }
 }
